b22d1328a1934f0a44933abde90acc8331006a91
zhanglingfe <771152260@qq.com>
2016-09-19 17:17:31 +0800

    fix

--- app/common/component/footer/index.js
+++ app/common/component/footer/index.js
@@ -2,33 +2,14 @@ import React from 'react'
 import { Link } from 'react-router'
 import './style.sass'
 
-export default React.createClass ({
-   	getInitialState() {
-   		return {
-   			footers: [{
-   				pathname: '/',
-   				text: '首页'
-   			}, {
-   				pathname: '/goods',
-   				text: '校园购'
-   			}, {
-   				pathname: '/community',
-   				text: '最美玩'
-   			}, {
-   				pathname: '/order',
-   				text: '购物车'
-   			}, {
-   				pathname: '/user',
-   				text: '我的'
-   			}]
-   		}
-   	},
+import { footers } from '../../../config.js'
 
+export default React.createClass ({
    	/* 给当前 tab 加 class 'active' */
    	isCurrent(index) {
    		// 取当前 pathname 的索引 currentIndex
    		let pathnameArr = [];
-   		this.state.footers.map((item, index) => {
+   		footers.map((item, index) => {
 			pathnameArr.push(item.pathname)
 		})
    		let currentIndex = pathnameArr.indexOf(location.pathname);
@@ -42,7 +23,8 @@ export default React.createClass ({
 	    return <footer
 	    	>
 			<ul>
-				{ this.state.footers.map((item, index) => {
+				{ footers.map((item, index) => {
+					console.log(footers)
 					return <li
 							className={ this.isCurrent(index) }
 						>
---
+++ app/config.js
@@ -0,0 +1,22 @@
+/**
+ * 全局配置
+ */
+
+export default {
+	footers: [{
+   				pathname: '/',
+   				text: '首页'
+   			}, {
+   				pathname: '/goods',
+   				text: '校园购'
+   			}, {
+   				pathname: '/community',
+   				text: '最美玩'
+   			}, {
+   				pathname: '/order',
+   				text: '购物车'
+   			}, {
+   				pathname: '/user',
+   				text: '我的'
+   			}]
+}
